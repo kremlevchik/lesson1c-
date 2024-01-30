ace169c77dc022b983710e633afa685a1977403a
Ivan <kremlevchik98@mail.ru>
2024-01-31 00:42:00 +0300

    add lesson1:Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 5;
+int b = 6;
+int c = 98;
+int d = 50;
+int e = 40;
+
+int max = a;
+if (b > a)
+{
+    max = b;
+}
+if (c > b)
+{
+    max = c;
+}
+if (d > c)
+{
+    max = d;
+}
+if (e > d)
+{
+    max = e;
+}
+Console.WriteLine(max);
